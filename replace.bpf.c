99b48340998107fdb727bf46daaadf8813911f86
wellmaks <maks.bilski@gmail.com>
2024-04-25 23:10:12 +0200

    getting on the right path

--- replace.bpf.c
+++ replace.bpf.c
@@ -2,81 +2,100 @@
 #include <bpf/bpf_helpers.h>
 #include <bpf/bpf_tracing.h>
 
-#define MAX_FILENAME_LEN 256
-
-static __always_inline int my_strncmp(const char *s1, const char *s2, int n) {
-    for (int i = 0; i < n && s1[i] && s2[i]; i++) {
-        if (s1[i] != s2[i])
-            return s1[i] - s2[i];
-    }
-    return 0;
-}
-
 struct {
     __uint(type, BPF_MAP_TYPE_HASH);
-    __type(key, u32);    // PID as key
-    __type(value, int);  // File descriptor as value
-    __uint(max_entries, 1024);
-} pid_fd_map SEC(".maps");
+    __uint(max_entries, 8192);
+    __type(key, size_t);
+    __type(value, unsigned int);
+} map_fds SEC(".maps");
 
-SEC("kretprobe/__x64_sys_openat")
-int ret_sys_openat(struct pt_regs *ctx) {
-    int fd = PT_REGS_RC(ctx);
-    if (fd < 0)
+#define MAX_FILENAME_LEN 256
+const int filename_len = 15; 
+const char filename[] = "/etc/aaabbb.txt";
+
+const int pid_target = 0; 
+
+SEC("tracepoint/syscalls/sys_enter_openat")
+int tracepoint__syscalls__sys_enter_openat(struct trace_event_raw_sys_enter* ctx)
+{
+    u64 id = bpf_get_current_pid_tgid();
+    u32 pid = id >> 32;
+
+    if (pid_target && pid_target != pid)
         return 0;
 
-    u32 pid = bpf_get_current_pid_tgid() >> 32;
-    char filename[MAX_FILENAME_LEN];
-    bpf_probe_read_user_str(filename, sizeof(filename), (void*)PT_REGS_PARM2(ctx));
+    char check_filename[MAX_FILENAME_LEN];
+    bpf_probe_read_user(&check_filename, filename_len, (char*)ctx->args[1]);
 
-    if (my_strncmp(filename, "/etc/passwd", 11) == 0) {
-        int *existing_fd = bpf_map_lookup_elem(&pid_fd_map, &pid);
-        if (!existing_fd) {
-            bpf_map_update_elem(&pid_fd_map, &pid, &fd, BPF_ANY);
-            bpf_printk("Started tracking /etc/passwd open by PID %d with FD %d\n", pid, fd);
-        } else if (*existing_fd != fd) {
-            bpf_map_update_elem(&pid_fd_map, &pid, &fd, BPF_ANY);
-            bpf_printk("Updated tracking to new FD %d for PID %d\n", fd, pid);
-        }
+    // Check if the filename matches
+    for(int i = 0; i <= filename_len; i++){  
+        if (check_filename[i] != filename[i])
+            return 0;
     }
+
+    unsigned int zero = 0;
+    bpf_map_update_elem(&map_fds, &id, &zero, BPF_ANY);
+    bpf_printk("Match found for PID %d and filename %s\n", pid, check_filename);
+
     return 0;
 }
 
-SEC("kretprobe/__x64_sys_read")
-int kretprobe_sys_read(struct pt_regs *ctx) {
-    int bytes_read = PT_REGS_RC(ctx);
-    if (bytes_read <= 0 || PT_REGS_PARM1(ctx) == 0)  // Ignore FD 0
-        return 0;
-
-    int fd = PT_REGS_PARM1(ctx);
-    u32 pid = bpf_get_current_pid_tgid() >> 32;
-    int *tracked_fd = bpf_map_lookup_elem(&pid_fd_map, &pid);
-    if (!tracked_fd || *tracked_fd != fd) {
-        bpf_printk("PID %d with FD %d not tracked or does not match tracked FD %d\n", pid, fd, (tracked_fd ? *tracked_fd : -1));
+SEC("tp/syscalls/sys_exit_openat")
+int handle_openat_exit(struct trace_event_raw_sys_exit *ctx)
+{
+        size_t id = bpf_get_current_pid_tgid();
+    unsigned int* check = bpf_map_lookup_elem(&map_fds, &id);
+    if (check == 0) {
         return 0;
     }
+    int pid = id >> 32;
 
-    char fake_data[] = "fuck you";
-    int data_len = sizeof(fake_data) - 1;
-    if (bytes_read >= data_len) {
-        bpf_probe_write_user((void *)PT_REGS_PARM2(ctx), fake_data, data_len);
-        bpf_printk("Modified read data for PID %d, FD %d\n", pid, fd);
-    }
+    unsigned int fd = (unsigned int)ctx->ret;
+    bpf_map_update_elem(&map_fds, &id, &fd, BPF_ANY);
+
     return 0;
 }
 
-SEC("kretprobe/__x64_sys_close")
-int ret_sys_close(struct pt_regs *ctx) {
-    int fd = PT_REGS_PARM1(ctx);
-    u32 pid = bpf_get_current_pid_tgid() >> 32;
-    int *tracked_fd = bpf_map_lookup_elem(&pid_fd_map, &pid);
-    if (tracked_fd && *tracked_fd == fd) {
-        bpf_map_delete_elem(&pid_fd_map, &pid);
-        bpf_printk("Stopped tracking FD %d for PID %d on close\n", fd, pid);
+SEC("tp/syscalls/sys_enter_read")
+int handle_read_enter(struct trace_event_raw_sys_enter *ctx)
+{
+    size_t id = bpf_get_current_pid_tgid();
+    unsigned int* check = bpf_map_lookup_elem(&map_fds, &id);
+    if (check == 0) {
+        return 0;
     }
+    int pid = id >> 32;
+
+    unsigned int fd = *check;
+    if (fd != (unsigned int)ctx->args[0]) {
+        return 0;
+    }
+
+    bpf_printk("Read syscall by PID %d on FD %d\n", pid, fd);
+
+    return 0;
+
+}
+// modify the file so i can read the content of the file in the printk
+SEC("tp/syscalls/sys_exit_read")
+int handle_read_exit(struct trace_event_raw_sys_exit *ctx)
+{
+    size_t id = bpf_get_current_pid_tgid();
+    unsigned int* check = bpf_map_lookup_elem(&map_fds, &id);
+    if (check == 0) {
+        return 0;
+    }
+    int pid = id >> 32;
+
+    unsigned int fd = *check;
+    // Assuming ctx->ret is the number of bytes read, no need to compare it with fd
+    bpf_printk("Read syscall by PID %d on FD %d returned %d bytes\n", pid, fd, (int)ctx->ret);
+    
+
     return 0;
 }
 
 
-char LICENSE[] SEC("license") = "Dual BSD/GPL";
+char LICENSE[] SEC("license") = "GPL";
+
 
